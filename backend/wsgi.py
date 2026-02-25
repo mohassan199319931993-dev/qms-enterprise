3764f158e2bcd2f0d952223e6d7bf0339177e98a
mohassan199319931993-dev <mohassan199319931993@gmail.com>
2026-02-25 10:58:21 +0200

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,17 +1,9 @@
-import os
-import sys
-
-# Path
-_here = os.path.dirname(os.path.abspath(__file__))
-if _here not in sys.path:
-    sys.path.insert(0, _here)
-
-# Create Flask app فقط
 from app import create_app
 
 app = create_app()
-application = app
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port)
+# لو create_app بيرجع tuple
+if isinstance(app, tuple):
+    app = app[0]
+
+application = app
